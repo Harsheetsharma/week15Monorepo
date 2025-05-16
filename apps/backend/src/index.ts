924e78d717dc1fb42c1f69d3c4a400156ea87e2a
Harsheetsharma <harshitsharma8012@gmail.com>
2025-05-16 18:09:43 +0530

    common added

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { BACKEND_URL } from "@repo/common/config"
+import { BACKEND_URL } from "@repo/common/config";
 const app = express();
 console.log(BACKEND_URL);
 app.get("/", (req, res) => {
